fe5ea4b0903360276fb49b37cee25af7001e992e
Marcos Ojeda Escalante <marcos.ojeda.escalante@gmail.com>
2023-06-20 21:24:41 -0400

    recursos estáticos

--- indexAntiguo2.js
+++ indexAntiguo2.js
@@ -1,22 +1,20 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
-
-/* app.get('/', (request, response) => {
+app.get('/', (request, response) => {
     response.send('Esta es mi primera página');
 });
 
 app.get('/inicio', (request, response) => {
     response.send('Esta es mi página de inicio');
-}); */
+});
 
-/* app.get('/bienvenidos', (request, response) => {
+app.get('/bienvenidos', (request, response) => {
     response.send('Esta es mi página de bienvenida');
-}); */
+});
 
 app.get('*', (request, response) => {
-    response.sendFile(__dirname + '/public/404.html');
+    response.send('404 | Recurso no encontrado');
 });
 
 app.listen(8080);
